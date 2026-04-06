49d4ef101c692ff9e05d19d11aa9289b516398fa
BraydenAbel27 <brayden.abel12@icloud.com>
2026-04-06 14:52:10 -0500

    madlib

---
+++ Assignments/MadLib.py
@@ -0,0 +1,26 @@
+#One day in a [adjective] [place], there was a [adjective] [person].
+#On a random Friday night, the [person] decided to go to the [place] and [action].
+#The [person] saw his buddy [person] and said what's up.
+#They talked about [noun] and [noun] for a while then decided to go to [place].
+#[person] was pretty hungry so he ate a [food] and drank some [drink].
+#After that, [person] went home and [action].
+
+adjective1 = input("Enter an adjective: ")
+place1 = input("Enter a place: ")
+adjective2 = input("Enter another adjective: ")
+person1 = input("Enter a person's name: ")
+person2 = input("Enter another person's name: ")
+place2 = input("Enter another place: ")
+action1 = input("Enter an action: ")
+person3 = input("Enter another person's name: ")
+person4 = input("Enter another person's name: ")
+noun1 = input("Enter a noun: ")
+noun2 = input("Enter another noun: ")
+place3 = input("Enter another place: ")
+person5 = input("Enter another person's name: ")
+food = input("Enter a food: ")
+drink = input("Enter a drink: ")
+person6 = input("Enter another person's name: ")
+action2 = input("Enter another action: ")
+
+print("One day in a " + adjective1 + " " + place1 + ", there was a " + adjective2 + " " + person1 + ". On a random Friday night, " + person2 + " decided to go to the " + place2 + " and " + action1 + ". The " + person3 + " saw his buddy " + person4 + " and said what's up. They talked about " + noun1 + " and " + noun2 + " for a while then decided to go to " + place3 + ". " + person5 + " was pretty hungry so he ate a " + food + " and drank some " + drink + ". After that, " + person6 + " went home and " + action2 + "." )
